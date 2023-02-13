185f6f3332b87332a38512115e99dc4e35cb3023
nadine-shi <nadinshi@gmail.com>
2023-02-13 15:01:55 +0300

    Added owner files to the storage list

--- js/views/StorageView.js
+++ js/views/StorageView.js
@@ -7,6 +7,9 @@ const
 
 	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
 	App = require('%PathToCoreWebclientModule%/js/App.js'),
+	Routing = require('%PathToCoreWebclientModule%/js/Routing.js'),
+
+	LinksUtils = require('modules/FilesWebclient/js/utils/Links.js'),
 
 	CFolderView = require('modules/%ModuleName%/js/views/CFolderView.js')
 ;
@@ -14,21 +17,36 @@ const
 function CStorageView()
 {
 	this.type = 'shared';
-	this.displayName = ko.observable('shared');
 	this.ownersData = ko.observableArray([]);
+	this.currentOwner = ko.observable('');
 	this.currentPath = ko.observable('');
-	
-//	App.subscribeEvent('ReceiveAjaxResponse::after', (oParams) => {
-//		const requestParams = oParams.Request.Parameters;
-//		if ((oParams.Request.Module === 'Files' || oParams.Request.Module === 'SharedFilesCustomTreeView') &&
-//				oParams.Request.Method === 'GetFiles' &&
-//				requestParams.Type === this.type
-//		) {
-//			if (requestParams.Path === '' && requestParams.Pattern === '') {
-//				this.parseRootSharedResponse(oParams.Response);
-//			}
-//		}
-//	});
+
+	App.subscribeEvent('ReceiveAjaxResponse::before', (oParams) => {
+		const requestParams = oParams.Request.Parameters;
+		if ((oParams.Request.Module === 'Files' || oParams.Request.Module === 'SharedFilesCustomTreeView') &&
+				oParams.Request.Method === 'GetFiles' &&
+				requestParams.Type === this.type
+		) {
+			if (requestParams.Path === '' && requestParams.Pattern === '') {
+				this.parseRootSharedResponse(oParams.Response);
+			}
+			if (this.currentOwner() !== '') {
+				const rawItems = Types.pArray(oParams.Response && oParams.Response.Result && oParams.Response.Result.Items);
+				if (Array.isArray(rawItems)) {
+					oParams.Response.Result.Items = rawItems
+							.filter(item => !item.IsFolder && item.Owner === this.currentOwner());
+				}
+			}
+		}
+		if (oParams.Request.Module === 'Files' &&
+				oParams.Request.Method === 'GetStorages'
+		) {
+			if (Array.isArray(oParams.Response.Result)) {
+				oParams.Response.Result = oParams.Response.Result
+						.map(item => ({ ...item, HideInList: item.Type === this.type }));
+			}
+		}
+	});
 }
 
 CStorageView.prototype.ViewTemplate = '%ModuleName%_StorageView';
@@ -40,7 +58,7 @@ CStorageView.prototype.findFolderView = function (owner, fullPath) {
 			.find(folderView => folderView.fullPath() === fullPath);
 };
 
-CStorageView.prototype.parseGetFilesResponse = function (response) {
+CStorageView.prototype.parseRootSharedResponse = function (response) {
 	const rawItems = Types.pArray(response && response.Result && response.Result.Items);
 	const ownersData = [];
 
@@ -77,12 +95,17 @@ CStorageView.prototype.onShow = function () {
 		Pattern: '',
 		PathRequired: false
 	};
-	Ajax.send('%ModuleName%', 'GetFiles', parameters, this.parseGetFilesResponse, this);
+	Ajax.send('%ModuleName%', 'GetFiles', parameters);
 };
 
 CStorageView.prototype.onRoute = function (params) {
-//	const { Name, Path, PathParts, Search, Storage } = params
+//	const { Name, Path, PathParts, Search, Storage, Custom } = params
 	this.currentPath(params.Path);
+	this.currentOwner(params.Custom ? params.Custom.substr(12) : '');
+};
+
+CStorageView.prototype.routeOwnerFiles = function (owner) {
+	Routing.setHash(LinksUtils.getFiles(this.type, '', '', { prefix: 'sharedOwner.', value: owner }));
 };
 
 module.exports = new CStorageView();
