1fb7d429a5a1b7a5f312b49dbf6675f251bb7b63
nadine-shi <nadinshi@gmail.com>
2023-02-13 13:56:06 +0300

    Added subfolders to the storage list

--- js/views/CFolderView.js
+++ js/views/CFolderView.js
@@ -3,25 +3,79 @@
 const
 	ko = require('knockout'),
 
-	Types = require('%PathToCoreWebclientModule%/js/utils/Types.js')
+	Types = require('%PathToCoreWebclientModule%/js/utils/Types.js'),
+
+	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js')
 ;
+
+let CSubfolderView = null;
 
 /**
  * @constructor
  * @param {object} folderData
+ * @param {number} level
+ * @param {constructor} folderViewConstructor
  */
-function CFolderView(folderData)
+function CFolderView(folderData, level, folderViewConstructor)
 {
-	this.type = Types.pString(folderData.Type);
-	this.fullPath = Types.pString(folderData.FullPath);
-	this.name = Types.pString(folderData.Name);
+	CSubfolderView = folderViewConstructor;
+//	const { Actions, Content, ContentType, ETag, ExtendedProps, FullPath, GroupId, Hash, Id,
+//		Initiator, IsExternal, IsFolder, IsLink, LastModified, LinkType, LinkUrl, Name, OembedHtml,
+//		Owner, Path, Published, Shared, Size, Type } = folderData;
 
+	this.level = level;
+	this.type = ko.observable('');
+	this.fullPath = ko.observable('');
+	this.name = ko.observable('');
+	this.parse(folderData);
+
+	this.subfolders = ko.observableArray([]);
 	this.expanded = ko.observable(false);
 }
+
+CFolderView.prototype.ViewTemplate = '%ModuleName%_FolderView';
+
+CFolderView.prototype.parse = function (folderData) {
+	this.type(Types.pString(folderData.Type));
+	this.fullPath(Types.pString(folderData.FullPath));
+	this.name(Types.pString(folderData.Name));
+};
+
+CFolderView.prototype.parseGetFilesResponse = function (response) {
+	if (!CSubfolderView) {
+		return;
+	}
+
+	const rawItems = Types.pArray(response && response.Result && response.Result.Items);
+	const subfolders = [];
+
+	rawItems.forEach(folderData => {
+		if (folderData.IsFolder) {
+			const folderView = this.subfolders().find(folderView => folderView.fullPath() === folderData.FullPath);
+			if (folderView) {
+				folderView.parse(folderData);
+				subfolders.push(folderView);
+			} else {
+				subfolders.push(new CSubfolderView(folderData, this.level + 1, CSubfolderView));
+			}
+		}
+	});
+
+	this.subfolders(subfolders);
+};
 
 CFolderView.prototype.toggleExpanded = function (view, e) {
 	e.stopPropagation();
 	this.expanded(!this.expanded());
+	if (this.expanded()) {
+		const parameters = {
+			Type: this.type(),
+			Path: this.fullPath(),
+			Pattern: '',
+			PathRequired: false
+		};
+		Ajax.send('%ModuleName%', 'GetFiles', parameters, this.parseGetFilesResponse, this);
+	}
 };
 
 module.exports = CFolderView;
--- js/views/StorageView.js
+++ js/views/StorageView.js
@@ -6,6 +6,7 @@ const
 	Types = require('%PathToCoreWebclientModule%/js/utils/Types.js'),
 
 	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
+	App = require('%PathToCoreWebclientModule%/js/App.js'),
 
 	CFolderView = require('modules/%ModuleName%/js/views/CFolderView.js')
 ;
@@ -16,9 +17,58 @@ function CStorageView()
 	this.displayName = ko.observable('shared');
 	this.ownersData = ko.observableArray([]);
 	this.currentPath = ko.observable('');
+	
+//	App.subscribeEvent('ReceiveAjaxResponse::after', (oParams) => {
+//		const requestParams = oParams.Request.Parameters;
+//		if ((oParams.Request.Module === 'Files' || oParams.Request.Module === 'SharedFilesCustomTreeView') &&
+//				oParams.Request.Method === 'GetFiles' &&
+//				requestParams.Type === this.type
+//		) {
+//			if (requestParams.Path === '' && requestParams.Pattern === '') {
+//				this.parseRootSharedResponse(oParams.Response);
+//			}
+//		}
+//	});
 }
 
 CStorageView.prototype.ViewTemplate = '%ModuleName%_StorageView';
+
+CStorageView.prototype.findFolderView = function (owner, fullPath) {
+	const ownerData = this.ownersData()
+			.find(ownerData => ownerData.owner === owner);
+	return ownerData && ownerData.folders
+			.find(folderView => folderView.fullPath() === fullPath);
+};
+
+CStorageView.prototype.parseGetFilesResponse = function (response) {
+	const rawItems = Types.pArray(response && response.Result && response.Result.Items);
+	const ownersData = [];
+
+	rawItems.forEach(folderData => {
+		let ownerData = ownersData.find(data => data.owner === folderData.Owner);
+		if (!ownerData) {
+			ownerData = {
+				owner: folderData.Owner,
+				folders: [],
+				hasFiles: false
+			};
+			ownersData.push(ownerData);
+		}
+		if (folderData.IsFolder) {
+			const folderView = this.findFolderView(folderData.Owner, folderData.FullPath);
+			if (folderView) {
+				folderView.parse(folderData);
+				ownerData.folders.push(folderView);
+			} else {
+				ownerData.folders.push(new CFolderView(folderData, 1, CFolderView));
+			}
+		} else {
+			ownerData.hasFiles = true;
+		}
+	});
+
+	this.ownersData(ownersData);
+};
 
 CStorageView.prototype.onShow = function () {
 	const parameters = {
@@ -27,30 +77,7 @@ CStorageView.prototype.onShow = function () {
 		Pattern: '',
 		PathRequired: false
 	};
-
-	Ajax.send('%ModuleName%', 'GetFiles', parameters, (response) => {
-		const rawItems = Types.pArray(response && response.Result && response.Result.Items);
-		const ownersData = [];
-
-		rawItems.forEach(folderData => {
-			let ownerData = ownersData.find(data => data.owner === folderData.Owner);
-			if (!ownerData) {
-				ownerData = {
-					owner: folderData.Owner,
-					folders: [],
-					hasFiles: false
-				};
-				ownersData.push(ownerData);
-			}
-			if (folderData.IsFolder) {
-				ownerData.folders.push(new CFolderView(folderData));
-			} else {
-				ownerData.hasFiles = true;
-			}
-		});
-
-		this.ownersData(ownersData);
-	});
+	Ajax.send('%ModuleName%', 'GetFiles', parameters, this.parseGetFilesResponse, this);
 };
 
 CStorageView.prototype.onRoute = function (params) {
